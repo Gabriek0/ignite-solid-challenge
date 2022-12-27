18c6271e69f2096095449ca3d24e882138d9b572
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-26 22:27:43 -0300

    feat: create a documentation for user route

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
+import { serve, setup } from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", serve, setup(swaggerDocument));
 
 app.use("/users", usersRoutes);
 
